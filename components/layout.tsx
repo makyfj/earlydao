a8687c6b4b330a3df7df65a12d52d49216044fbd
nmandal <nicholas.mandal@gmail.com>
2022-09-12 12:25:29 -0500

    no beam logo

--- components/layout.tsx
+++ components/layout.tsx
@@ -31,9 +31,12 @@ export function Layout({ children }: LayoutProps) {
     <div className="max-w-3xl px-6 mx-auto">
       <header className="flex items-center justify-between gap-4 py-12 md:py-20">
         <Link href="/">
-          <a>
+          {/* <a>
             <Logo className="w-auto text-red-light h-[34px]" />
-          </a>
+          </a> */}
+          <h2 className="text-2xl font-bold tracking-tight md:text-3xl">
+            EARLY
+          </h2>
         </Link>
         <div className="flex items-center gap-2 md:gap-4">
           <IconButton
