4db968d09eb5086397844b7166c7857992ce9ffb
nmandal <nicholas.mandal@gmail.com>
2022-10-02 06:12:03 -0500

    favicon in document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,10 +19,24 @@ class MyDocument extends Document {
             href="/fonts/inter-italic.var.woff2"
             crossOrigin="anonymous"
           />
-          <link rel="icon" href="/favicon.ico" sizes="any" />
-          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-          <link rel="manifest" href="/manifest.webmanifest" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
           <meta name="theme-color" content="#262732" />
         </Head>
         <body>
